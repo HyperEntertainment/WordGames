4d772682dc216e1c825f3cd33cc857e23db4acf2
Derek M <d1973@rocketmail.com>
2018-12-31 19:46:24 -0800

    Creating the skeleton server...

---
+++ WordGames/WordGames/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WordGames.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WordGames/WordGames/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WordGames.WebApiApplication" Language="C#" %>
