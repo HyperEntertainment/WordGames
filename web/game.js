ff70d26455ba7cc23fc85241513b9c6b8492a29b
GreenOxen <GreenOxen@users.noreply.github.com>
2019-01-01 01:35:22 -0500

    initial typing handling

--- web/game.js
+++ web/game.js
@@ -1,25 +1,81 @@
 
+// background globals
 var canvas = null;
 var ctx = null;
 var width;
 var height;
 var ws = null; 
+var timerId;
+var frame = 0;
 
-function drawText(x,y,text)
+var BLACK = "#000000";
+var RED = "#FF0000";
+var GREEN = "#00FF00";
+var BLUE = "#0000FF";
+var WHITE = "#FFFFFF";
+
+// game globals
+var prevText="";
+var typeText="";
+
+
+function drawText(x,y,clr,text)
 {
-    ctx.fillStyle = "#FF00FF";
+    ctx.fillStyle = clr;
     ctx.fillText(text,x,y);
 };
 
+function drawTyping()
+{
+    ctx.fillStyle = BLACK;
+    var w = 10 + ctx.measureText(prevText+'_').width;
+    ctx.fillRect(0,5,w,30);
+    prevText = typeText;
+    drawText(5,5,WHITE,typeText+"_");
+}
+
 function draw()
 {
-    ctx.fillStyle = "#000000";
+    ctx.fillStyle = BLACK;
     ctx.fillRect(0,0,width,height);
-    drawText(200,50,"Hello World");
+
+    drawTyping();    
+
+    drawText(200,50,RED,"Hello World");
 };
 
+function keydown(evt)
+{
+    if(evt.key.length==1){
+        typeText += evt.key;
+        drawTyping();
+    }
+    else if(evt.key == 'Backspace'){
+        if(typeText.length){
+            typeText = typeText.substr(0,typeText.length-1);
+            drawTyping();
+        }
+    }
+    else if(evt.key == 'Enter'){
+        // do something with it?
 
-function start()
+        typeText = "";
+        drawTyping();
+    }
+    else {
+        console.log(evt);
+    }
+
+    return true;
+}
+
+function interval()
+{
+    draw();
+    frame++;
+}
+
+function init()
 {
     if (!("WebSocket" in window)) {
         // The browser doesn't support WebSocket
@@ -32,6 +88,7 @@ function start()
     height = canvas.height;
     ctx = canvas.getContext("2d");
     ctx.font = "30px Arial";
+    ctx.textBaseline = 'top';
 
     var ws = new WebSocket("ws://localhost:8765/echo");
         
@@ -50,10 +107,14 @@ function start()
        console.log("Connection closed");
     };
 
-    draw();
+    //document.onkeypress
+    document.onkeydown = keydown;
+
+    timerId = setInterval(interval,2000);
+
+    //console.log("end init()");
 };
 
-window.onload = start;
-
-console.log("here");
+window.onload = init;
+//console.log("end game.js");
 
